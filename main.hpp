105abc7ce15e19810990bff7f454c2a9991c49a9
Қалдыбай Алихан <Qaldybay_A06@krg.nis.edu.kz>
2026-09-25 17:11:24 +0500

    Added comments

--- main.hpp
+++ main.hpp
@@ -9,9 +9,20 @@
 #define LOG
 #endif
 
+/**
+ * @brief Just a database. Look at methods brief if you want to know more.
+ */
 class DataBase{
     public:
+    /**
+     * @brief Just fields of database.
+     * @note Storing types with pointer as field or with vtable is NOT RECOMENDED
+     */
     std::map<std::string,std::vector<std::uint8_t>> attrs;
+    /**
+     * @brief Constructor with path to database.
+     * @param path Path to database.
+     */
     DataBase(const std::string& path){
         std::ifstream file(path,std::ios::binary);
         while (file.peek() != std::ifstream::traits_type::eof()){
@@ -32,7 +43,14 @@ class DataBase{
         }
     }
 
+    /**
+     * @brief Default constructor with no parameters.
+     */
     DataBase()=default;
+    /**
+     * @brief Dumps whole database to file.
+     * @param path Path to database.
+     */
     void WriteTo(const std::string& path){
         std::ofstream file(path,std::ios::binary | std::ios::in);
         for (auto [k,v]:attrs){
@@ -48,18 +66,34 @@ class DataBase{
             file.write(data,size2);
         }
     }
+    /**
+     * @brief Adds integer to DataBase::atrs eventually turning it into vector of uint8_t
+     * @param key Key.
+     * @param num Integer to be stored.
+     */
     void AddInt(const std::string& key, uint32_t num){
         std::vector<uint8_t> arr;
         arr.resize(4);
         memcpy(arr.data(), &num, sizeof(uint32_t));
         attrs[key]=std::move(arr);
     }
+    /**
+     * @brief Adds string to DataBase::atrs eventually turning it into vector of uint8_t
+     * @param key Key.
+     * @param str String to be stored.
+     */
     void AddStr(const std::string& key, const std::string& str){
         std::vector<uint8_t> arr;
         arr.resize(str.size());
         memcpy(arr.data(), str.data(), str.size());
         attrs[key]=std::move(arr);
     }
+    /**
+     * @brief Adds vector to DataBase::atrs eventually turning it into vector of uint8_t
+     * @param key Key.
+     * @param vec Vector to be stored.
+     * @param T Template parameter which is type of a vector.
+     */
     template <typename T>
     void AddVector(const std::string& key, const std::vector<T>& vec){
         std::vector<uint8_t> arr;
@@ -67,11 +101,24 @@ class DataBase{
         memcpy(arr.data(), vec.data(), vec.size()*sizeof(T));
         attrs[key]=std::move(arr);
     }
+    /**
+     * @brief Reads bytes and converts it to std::vector.
+     * @param key Key.
+     * @param T Type of std::vector
+     * @return Vector with T type
+     */
     template <typename T>
     std::vector<T> ReadVector(const std::string& key){
         std::vector<T> l(attrs[key].size()/sizeof(T));
         memcpy(l.data(), attrs[key].data(), attrs[key].size());
     }
+    /**
+     * @brief Adds map to DataBase::atrs eventually turning it into vector of uint8_t
+     * @param key Key.
+     * @param map Map to be stored.
+     * @param K Type of key
+     * @param V Type of value
+     */
     template <typename K, typename V>
     void AddMap(const std::string& key, std::map<K,V> map){
         std::vector<uint8_t> arr;
@@ -85,6 +132,12 @@ class DataBase{
         }
         attrs[key]=std::move(arr);
     }
+    /**
+     * @brief Reads bytes and converts it to std::vector.
+     * @param key Key.
+     * @param K Type of key
+     * @param V Type of value
+     */
     template <typename K, typename V>
     std::map<K,V> ReadMap(const std::string& key){
         const std::vector<uint8_t>& arr=attrs[key];
